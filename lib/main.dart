85c46d08f7deccb76615ad534d7140f3b1b156d0
Samuel Ademujimi <samshotmedia01@gmail.com>
2023-09-11 23:49:54 +0100

    feat : Project completed

---
+++ lib/main.dart
@@ -0,0 +1,24 @@
+import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:hng_stage1/profile_screen.dart';
+
+void main() {
+  runApp(const ProfileApp());
+}
+
+class ProfileApp extends StatelessWidget {
+  const ProfileApp({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      debugShowCheckedModeBanner: false,
+      theme: ThemeData(
+        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
+        useMaterial3: true,
+        textTheme: GoogleFonts.manropeTextTheme()
+      ),
+      home: const ProfileScreen(),
+    );
+  }
+}
